3044efd17a129d4e530665981924a99575147938
jorikdima <jorikdima@gmail.com>
2017-06-30 16:42:53 -0700

    Update on tmpmeas

---
+++ apps/tempmeas/tmpmeas.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+
+import sys
+import os
+import time
+import csv
+import datetime
+
+class ADC:
+
+    chan = 1
+    adcpath = 'testadc'  # '/dev/adc'
+    adcfile = None
+
+    def read(self):
+        if self.adcfile is not None:
+            val = int(self.adcfile.readline())
+            self.adcfile.seek(0)
+            return val
+
+    def __init__(self, channum):
+        self.chan = channum
+
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        fname = dir_path + os.path.sep + self.adcpath
+
+        print("ADC file: ", fname)
+
+        self.adcfile = open(fname, 'r')
+
+
+def main(argv):
+    print("Start")
+    adc = ADC(1)
+
+    with open('tprofile.csv', 'r') as f:
+        reader = csv.reader(f)
+        tprofile = dict(reader)
+        tprofile = {int(old_key): float(val) for old_key, val in tprofile.items()}
+        del reader
+
+    
+    start = datetime.datetime.now()
+
+    while True:
+        v = adc.read()
+
+        dt = datetime.datetime.now() - start
+        dt = dt.seconds
+        
+        tp = tprofile[dt] if dt in tprofile
+      
+        
+        print('ADC: %d  Profile: %d  %d' % (v, tp, dt))
+        time.sleep(0.5)
+
+
+if __name__ == "__main__":
+    main(sys.argv)
