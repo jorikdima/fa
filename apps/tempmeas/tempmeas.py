f33400c5db179abaaea0ccc0476cd4255eb487b9
Dmitry Kuznetsov <jorikdima@gmail.com>
2017-07-08 20:35:34 -0700

    Minor changes

--- apps/tempmeas/tempmeas.py
+++ apps/tempmeas/tempmeas.py
@@ -8,10 +8,11 @@ import datetime
 import threading
 import select
 
+
 class Button:
 
     last_pressed = None
-    
+
     # Pin to work with
     pin = '144'
     pin_file = None
@@ -20,7 +21,7 @@ class Button:
     def write_once(path, value):
         with open(path, 'w') as f:
             f.write(value)
-    
+
     def __init__(self, channum):
         path = '/sys/class/gpio/'
         self.pin_file = os.path.join(path, 'gpio' + self.pin)
@@ -33,24 +34,24 @@ class Button:
         Button.write_once(os.path.join(self.pin_file, 'edge'), 'falling')
 
         self.f = open(os.path.join(self.pin_file, 'value'), 'r')
-                
+
         t = threading.Thread(target=self.poller, daemon=True)
         t.start()
-        
+
     def poller(self):
-        
+
         po = select.poll()
         po.register(self.f, select.POLLPRI)
         self.f.read()
 
         while True:
             events = po.poll(10000)
-            if events:            
+            if events:
                 self.f.seek(0)
                 self.f.read()
                 self.last_pressed = datetime.datetime.now()
                 print("Starting over...")
-                
+
 
 class ADC:
 
@@ -61,7 +62,8 @@ class ADC:
     def read(self):
         if self.adcfile is not None:
             val = int(self.adcfile.readline())
-            if DEBUG_MODE: self.adcfile.seek(0)
+            if DEBUG_MODE:
+                self.adcfile.seek(0)
             return val
 
     def __init__(self, channum):
@@ -75,12 +77,11 @@ class ADC:
             self.fname = self.adcpath
 
         print("ADC file: ", self.fname)
-     
 
-    def __enter__(self):        
+    def __enter__(self):
         self.adcfile = open(self.fname, 'r')
- 
-    def __exit__(self, exc_type, exc_value, traceback):        
+
+    def __exit__(self, exc_type, exc_value, traceback):
         self.adcfile.close()
 
 
@@ -98,15 +99,16 @@ def main(argv):
 
     button = Button(1)
 
-    tprofile = os.path.dirname(os.path.realpath(__file__)) + os.path.sep + 'tprofile.csv'
+    tprofile = os.path.dirname(os.path.realpath(__file__))
+    + os.path.sep + 'tprofile.csv'
 
     with open(tprofile, 'r') as f:
         reader = csv.reader(f)
         tprofile = dict(reader)
-        tprofile = {int(old_key): float(val) for old_key, val in tprofile.items()}
+        tprofile = {int(old_key): float(val)
+                    for old_key, val in tprofile.items()}
         del reader
 
-        
     tp = 0
 
     adc = ADC(1)
@@ -118,12 +120,12 @@ def main(argv):
 
             if button.last_pressed is not None:
                 dt = (datetime.datetime.now() - button.last_pressed).seconds
-                if dt in tprofile: tp = tprofile[dt]      
-            
+                if dt in tprofile:
+                    tp = tprofile[dt]
+
             print('ADC: %d  Profile: %d  %d' % (v, tp, dt))
             time.sleep(0.5)
 
 
 if __name__ == "__main__":
     main(sys.argv[1:])
-
